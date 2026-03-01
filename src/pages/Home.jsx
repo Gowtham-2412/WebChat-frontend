0ab9394f282b20d079376dcbe1c7777ce5c64bba
gowtham <sgowtham.konijeti@gmail.com>
2026-03-01 19:31:10 +0530

    UX change 2

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,37 +1,35 @@
 import { useContext } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Navigate } from "react-router-dom";
 import { AuthContext } from "../context/AuthContext";
 
 const Home = () => {
     const navigate = useNavigate();
-    const { isAuthenticated } = useContext(AuthContext)
+    const { isAuthenticated } = useContext(AuthContext);
+
+    if (isAuthenticated) {
+        return <Navigate to="/chat" replace />;
+    }
 
     return (
-        <>
-        {
-            isAuthenticated ? navigate('/chat'):
-            <div className="h-screen flex flex-col items-center justify-center bg-gray-100">
-                <h1 className="text-4xl font-bold mb-6">Web Chat</h1>
+        <div className="h-screen flex flex-col items-center justify-center bg-gray-100">
+            <h1 className="text-4xl font-bold mb-6">Web Chat</h1>
 
-                <div className="space-x-4">
-                    <button
-                        onClick={() => navigate("/login")}
-                        className="bg-blue-500 text-white px-6 py-2 rounded"
-                    >
-                        Login
-                    </button>
+            <div className="space-x-4">
+                <button
+                    onClick={() => navigate("/login")}
+                    className="bg-blue-500 text-white px-6 py-2 rounded"
+                >
+                    Login
+                </button>
 
-                    <button
-                        onClick={() => navigate("/register")}
-                        className="bg-green-500 text-white px-6 py-2 rounded"
-                    >
-                        Register
-                    </button>
-
-                </div>
+                <button
+                    onClick={() => navigate("/register")}
+                    className="bg-green-500 text-white px-6 py-2 rounded"
+                >
+                    Register
+                </button>
             </div>
-        }
-        </>
+        </div>
     );
 }
 
